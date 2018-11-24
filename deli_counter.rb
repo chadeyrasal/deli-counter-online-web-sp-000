9e8a635b6ea23a5a1c28e1a026fbca335bd3af85
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-24 09:53:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
 end
 
 def now_serving
