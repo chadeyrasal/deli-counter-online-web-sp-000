e153847205fde3a934bbe3a7ab97c6d4838e4a3c
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-24 10:09:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  katz_deli = take_a_number(katz_deli)
+  katz_deli = take_a_number(katz_deli,name)
   if katz_deli = []
     puts "There is nobody waiting to be served!"
   else
